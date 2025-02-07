28a03b1a8b1bc802c1e2cd708312e6a38d4fba0f
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2025-02-07 11:41:44 -0300

    GO

---
+++ enrichmentGO.R
@@ -0,0 +1,82 @@
+library(clusterProfiler)
+library(org.Mm.eg.db)
+
+
+
+# AA x NT (Asfotase Alfa x Non-Treated) -----------------------------------
+# coef = 2
+
+AAxNT <- topTable(fit2,
+         coef = 2,#o contraste avaliado
+         adjust.method = "BH",#Benjamin hocheback - Falsos positivos corrigidos
+         number = Inf #numero de linhas para serem exibidas
+)
+
+AAxNT$UNIPROT <- rownames(AAxNT)
+
+AAxNT %>% 
+  mutate(Diff = case_when(
+    logFC < -1 & P.Value < 0.05 ~ "Down",
+    logFC > 1 & P.Value < 0.05 ~ "Up",
+    TRUE ~ "Ns"
+  )) %>% 
+  count(Diff)
+
+AAxNT_diff <- 
+  AAxNT %>% 
+  mutate(Diff = case_when(
+    logFC < -1 & P.Value < 0.05 ~ "Down",
+    logFC > 1 & P.Value < 0.05 ~ "Up",
+    TRUE ~ "Ns"
+  )) %>%
+  filter(Diff != "Ns") %>% 
+  pull(UNIPROT)
+
+annot_gene <- AnnotationDbi::select(x = org.Mm.eg.db,
+                                    keys = AAxNT_diff,
+                                    columns = c("SYMBOL","ENTREZID","UNIPROT"),
+                                    keytype = "UNIPROT")
+
+tmp_go <- enrichGO(gene = AAxNT_diff,
+         OrgDb = org.Mm.eg.db,
+         keyType = "UNIPROT",
+         ont = "ALL")
+
+# AA+CKD x AA -------------------------------------------------------------
+# coef = 4
+
+
+AA_CKDxAA <- topTable(fit2,
+                  coef = 4,#o contraste avaliado
+                  adjust.method = "BH",#Benjamin hocheback - Falsos positivos corrigidos
+                  number = Inf #numero de linhas para serem exibidas
+)
+
+AA_CKDxAA$UNIPROT <- rownames(AA_CKDxAA)
+
+AA_CKDxAA %>% 
+  mutate(Diff = case_when(
+    logFC < -1 & P.Value < 0.05 ~ "Down",
+    logFC > 1 & P.Value < 0.05 ~ "Up",
+    TRUE ~ "Ns"
+  )) %>% 
+  count(Diff)
+
+# AA+CKD x CKD ------------------------------------------------------------
+# coef = 5
+
+AA_CKD_vs_CKD <- topTable(fit2,
+                      coef = 5,#o contraste avaliado
+                      adjust.method = "BH",#Benjamin hocheback - Falsos positivos corrigidos
+                      number = Inf #numero de linhas para serem exibidas
+)
+
+AA_CKD_vs_CKD$UNIPROT <- rownames(AA_CKD_vs_CKD)
+
+AA_CKD_vs_CKD %>% 
+  mutate(Diff = case_when(
+    logFC < -1 & P.Value < 0.05 ~ "Down",
+    logFC > 1 & P.Value < 0.05 ~ "Up",
+    TRUE ~ "Ns"
+  )) %>% 
+  count(Diff)
