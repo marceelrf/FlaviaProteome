790968c9e6ce82495a8e2e1513fb579c752bb145
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2025-02-18 17:01:39 -0300

    update

--- gsea_conv.R
+++ gsea_conv.R
@@ -16,3 +16,34 @@ AA_CKDxAA_tibble <- AA_CKDxAA %>%
   left_join(mapping, by = join_by(UNIPROT == From)) %>% 
   select(-SYMBOL,-ENTREZID) %>% 
   rename(Symbol = To)
+
+
+
+# ORA ---------------------------------------------------------------------
+
+fn_ORA_gene_diff <- function(x, lfc = 1, p_valor = 0.05) {
+
+  x %>% 
+    drop_na() %>% 
+    dplyr::mutate(Diff = case_when(
+      logFC < -lfc & P.Value < p_valor ~ "Down",
+      logFC > lfc & P.Value < p_valor ~ "Up",
+      TRUE ~ "Ns"
+    )) %>%
+    dplyr::filter(Diff != "Ns") %>% 
+    pull(Symbol)
+}
+
+# fn_ORA_gene_diff(AA_CKDxAA_tibble)
+
+ora_result_AAxNT <- enricher(fn_ORA_gene_diff(AAxNT_tibble),
+                             TERM2GENE = msig_data)
+ora_result_AA_CKDxAA <- enricher(fn_ORA_gene_diff(AA_CKDxAA_tibble),
+                                 TERM2GENE = msig_data)
+ora_result_AA_CKD_vs_CKD <- enricher(fn_ORA_gene_diff(AA_CKD_vs_CKD_tibble),
+                                     TERM2GENE = msig_data)
+
+
+ora_result_AA_CKDxAA@result %>% 
+  mutate(qscore = -log(p.adjust, base=10)) %>% 
+  barplot(x="qscore")
